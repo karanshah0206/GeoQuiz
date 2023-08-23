9568eddbdcc1a47a6dc19bcbd3d9e56232cfb0d7
Karan Shah <karanshah0206@outlook.com>
2023-08-24 01:44:53 +1000

    Add Functionality To Buttons

--- app/src/main/java/com/example/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/geoquiz/QuizActivity.kt
@@ -2,10 +2,22 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class QuizActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        val mTrueButton = findViewById<Button>(R.id.true_button)
+        mTrueButton.setOnClickListener {
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+        }
+
+        val mFalseButton = findViewById<Button>(R.id.false_button)
+        mFalseButton.setOnClickListener {
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
     }
 }
